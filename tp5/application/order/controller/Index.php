4430a78147b96568dba2fd7ac37b9afae6be6823
evangeion <2507606249@qq.com>
2017-10-19 13:49:30 +0800

    修改了道具城个人中心相关页面

--- tp5/application/order/controller/Index.php
+++ tp5/application/order/controller/Index.php
@@ -21,4 +21,44 @@ class Index extends Controller
 	{
 		return $this->fetch();
 	}
+	public function actual_trade()
+	{
+		return $this->fetch();
+	}
+	public function myaddress()
+	{
+		return $this->fetch();
+	}
+	public function ticket()
+	{
+		return $this->fetch();
+	}
+	public function coupon()
+	{
+		return $this->fetch();
+	}
+	public function coin()
+	{
+		return $this->fetch();
+	}
+	public function message()
+	{
+		return $this->fetch();
+	}
+	public function attention()
+	{
+		return $this->fetch();
+	}
+	public function prop()
+	{
+		return $this->fetch();
+	}
+	public function charm()
+	{
+		return $this->fetch();
+	}
+	public function myticket()
+	{
+		return $this->fetch();
+	}
 }
